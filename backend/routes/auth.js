2856bf5a3f052a04a7caf4546d2dce594fea4e7a
yashodhanAgale <yashodhanagale3@gmail.com>
2024-10-01 14:06:56 +0530

    fetched questions

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { signup } = require("../controllers/authController");
 const { login } = require("../controllers/authController"); // Import the login controller
+const { getQuestions } = require("../controllers/getQuestions");
 const router = express.Router();
 
 // POST route for signup
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/questions", getQuestions);
 
 module.exports = router;
